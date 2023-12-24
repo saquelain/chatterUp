393fc6b0f16935f6ff3f6e99bda9007d7d2f9dc0
saquelain mokhtar <saquelain1502@gmail.com>
2023-12-25 02:48:30 +0530

    add frontend-file

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 import express from 'express';
 import http from 'http';
+import path from 'path';
 import { Server } from 'socket.io';
 import cors from 'cors';
 import { connectToDatabase } from './db.config.js';
 import { chatModel } from './message.schema.js';
 import { userModel } from './user.schema.js';
-
+const __filename = new URL(import.meta.url).pathname;
+const __dirname = path.dirname(__filename);
 export const app = express();
 app.use(cors());
 
 export const server = http.createServer(app);
+const publicDirectoryPath = path.join(__dirname, '../public');
 
 const io = new Server(server, {
     cors: {
