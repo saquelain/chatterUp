ac2dc0fa45e52aab39b9186ce1015ed16e8a559f
saquelain mokhtar <saquelain1502@gmail.com>
2023-12-25 05:23:34 +0530

    final database config issue resolved

--- db.config.js
+++ db.config.js
@@ -3,13 +3,13 @@ dotenv.config();
 import mongoose from 'mongoose';
 
 const baseUrl = process.env.MONGODB || '0.0.0.0:27017';
+const url = process.env.DB_URL;
 
 export const connectToDatabase = async () => {
     try {
-        await mongoose.connect(`mongodb://${baseUrl}/Chatterup`, {
+        await mongoose.connect(url, {
             useNewUrlParser: true,
-            useUnifiedTopology: true,
-            family: 6
+            useUnifiedTopology: true
         });
         console.log("MongoDB connected using mongoose");
     } catch (err) {
