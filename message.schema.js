d9207e9071733ac0a2a1c0df6b9c3c5b999d2de0
saquelain mokhtar <saquelain1502@gmail.com>
2023-12-25 02:29:46 +0530

    userSchema, messageSchema create

---
+++ message.schema.js
@@ -0,0 +1,13 @@
+// make the necessary imports here
+import mongoose from 'mongoose';
+// implement the below schema
+
+const messageSchema = new mongoose.Schema({
+    username: String,
+    text: String,
+    room: String,
+    timestamp: Date,
+});
+
+
+export const chatModel = mongoose.model('Message', messageSchema);
