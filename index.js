5d523767fce063e671c35227cde37636e26ac07c
saquelain mokhtar <saquelain1502@gmail.com>
2023-12-25 01:22:29 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import http from 'http';
+import { Server } from 'socket.io';
+import cors from 'cors';
+
+export const app = express();
+app.use(cors());
+
+export const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: {
+        origin: '*',
+        methods: ["GET", "POST"]
+    }
+});
+
+
+server.listen(3000, ()=>{
+    console.log(`Server is listening on port 3000`);
+})
