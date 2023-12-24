548db60ef31547c56fdb2d65da339ec6a6f0e045
saquelain mokhtar <saquelain1502@gmail.com>
2023-12-25 03:20:53 +0530

    render html files

--- index.js
+++ index.js
@@ -6,13 +6,20 @@ import cors from 'cors';
 import { connectToDatabase } from './db.config.js';
 import { chatModel } from './message.schema.js';
 import { userModel } from './user.schema.js';
-const __filename = new URL(import.meta.url).pathname;
+import { fileURLToPath } from 'url';
+
+// Serve static files from the 'public' folder
+const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
 export const app = express();
 app.use(cors());
 
+const publicPath = path.join(__dirname, 'public');
+app.use(express.static(publicPath));
+
+
 export const server = http.createServer(app);
-const publicDirectoryPath = path.join(__dirname, '../public');
 
 const io = new Server(server, {
     cors: {
